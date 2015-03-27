fd4bd433f3438d79a9ac4d0d11627da1e15555c6
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-03-27 15:05:32 +0000

    Alignment fix and comments added

--- main.c
+++ main.c
@@ -20,27 +20,27 @@
 
 struct chairs
 {
-    struct customer **customer; /* Array of customers */
-    int max;    // Number of chairs in waiting room
-    int front;  // First available chair
-    int rear;   // Back of buffer
-    sem_t mutex; // Protects access to chairs
-    sem_t slots; // Number of available chairs
-    sem_t items; // Number of occupied chairs
+  struct customer **customer; /* Array of customers */
+  int max;    // Number of chairs in waiting room
+  int front;  // First available chair
+  int rear;   // Back of buffer
+  sem_t mutex; // Protects access to chairs
+  sem_t slots; // Number of available chairs
+  sem_t items; // Number of occupied chairs
 };
 
 struct barber
 {
-    int room;
-    struct simulator *simulator;
+  int room;
+  struct simulator *simulator;
 };
 
 struct simulator
 {
-    struct chairs chairs;
-    
-    pthread_t *barberThread;
-    struct barber **barber;
+  struct chairs chairs;
+
+  pthread_t *barberThread;
+  struct barber **barber;
 };
 
 
@@ -49,49 +49,49 @@ struct simulator
  */
 static void customer_arrived(struct customer *customer, void *arg)
 {
-    struct simulator *simulator = arg;
-    struct chairs *chairs = &simulator->chairs;
+  struct simulator *simulator = arg;
+  struct chairs *chairs = &simulator->chairs;
 
-    sem_init(&customer->mutex, 0, 0);
+  sem_init(&customer->mutex, 0, 0);
 
-    if(sem_trywait(&chairs->slots) == -1) {
-        // No chair available, customer leaves.
-        thrlab_reject_customer(customer);
-    } else {  
-        sem_wait(&chairs->mutex); // Lock the chairs
+  if (sem_trywait(&chairs->slots) == -1) {
+    // No chair available, customer leaves.
+    thrlab_reject_customer(customer);
+  } else {
+    sem_wait(&chairs->mutex); // Lock the chairs
 
-        thrlab_accept_customer(customer);
-        // Choose a seat for the customer
-        chairs->customer[(++chairs->rear) % (chairs->max)] = customer; 
+    thrlab_accept_customer(customer);
+    // Choose a seat for the customer
+    chairs->customer[(++chairs->rear) % (chairs->max)] = customer;
 
-        sem_post(&chairs->mutex); // Unlock the chairs
-        sem_post(&chairs->items); // Announce available customer
-        sem_wait(&customer->mutex);
-    }
+    sem_post(&chairs->mutex); // Unlock the chairs
+    sem_post(&chairs->items); // Announce available customer
+    sem_wait(&customer->mutex);
+  }
 }
 
 static void *barber_work(void *arg)
 {
-    struct barber *barber = arg;
-    struct chairs *chairs = &barber->simulator->chairs;
-    struct customer *customer;
+  struct barber *barber = arg;
+  struct chairs *chairs = &barber->simulator->chairs;
+  struct customer *customer;
 
-    /* Main barber loop */
-    while (true) {
-        sem_wait(&chairs->items); /* Wait for available item */
-        sem_wait(&chairs->mutex); /* Lock the buffer */
-        
-        customer = chairs->customer[(++chairs->front) % (chairs->max)];  /* Remove the item */
+  /* Main barber loop */
+  while (true) {
+    sem_wait(&chairs->items); /* Wait for available item */
+    sem_wait(&chairs->mutex); /* Lock the buffer */
 
-        sem_post(&chairs->mutex); /* Unlock the buffer */
-        sem_post(&chairs->slots); /* Announce available slot */
-	
-        thrlab_prepare_customer(customer, barber->room);
-        thrlab_sleep(5 * (customer->hair_length - customer->hair_goal));
-        thrlab_dismiss_customer(customer, barber->room);
-        sem_post(&customer->mutex);
-    }
-    return NULL;
+    customer = chairs->customer[(++chairs->front) % (chairs->max)];  /* Remove the item */
+
+    sem_post(&chairs->mutex); /* Unlock the buffer */
+    sem_post(&chairs->slots); /* Announce available slot */
+
+    thrlab_prepare_customer(customer, barber->room);
+    thrlab_sleep(5 * (customer->hair_length - customer->hair_goal));
+    thrlab_dismiss_customer(customer, barber->room);
+    sem_post(&customer->mutex);
+  }
+  return NULL;
 }
 
 /**
@@ -121,43 +121,43 @@ static void setup(struct simulator *simulator)
   struct barber *barber;
 
   for (unsigned int i = 0; i < thrlab_get_num_barbers(); i++)
-    {
-      barber = calloc(sizeof(struct barber), 1);
-      barber->room = i;
-      barber->simulator = simulator;
-      simulator->barber[i] = barber;
-      if (pthread_create(&simulator->barberThread[i], 0, barber_work, barber) != 0) {
-      	fprintf(stderr, "Could not create barber thread\n");
-      };
-      if (pthread_detach(simulator->barberThread[i]) != 0) {
-        fprintf(stderr, "Could not detach barber thread\n");
-      };
-    }
+  {
+    barber = calloc(sizeof(struct barber), 1);
+    barber->room = i;
+    barber->simulator = simulator;
+    simulator->barber[i] = barber;
+    if (pthread_create(&simulator->barberThread[i], 0, barber_work, barber) != 0) {
+      fprintf(stderr, "Could not create barber thread\n");
+    };
+    if (pthread_detach(simulator->barberThread[i]) != 0) {
+      fprintf(stderr, "Could not detach barber thread\n");
+    };
+  }
 }
 /**
  * Free all used resources and end the barber threads.
  */
 static void cleanup(struct simulator *simulator)
 {
-    /* Free chairs */
-    free(simulator->chairs.customer);
+  /* Free chairs */
+  free(simulator->chairs.customer);
 
-    /* Free barber thread data */
-    free(simulator->barber);
-    free(simulator->barberThread);
+  /* Free barber thread data */
+  free(simulator->barber);
+  free(simulator->barberThread);
 }
 
 int main (int argc, char **argv)
 {
-    struct simulator simulator;
+  struct simulator simulator;
 
-    thrlab_setup(&argc, &argv);
-    setup(&simulator);
+  thrlab_setup(&argc, &argv);
+  setup(&simulator);
 
-    thrlab_wait_for_customers(customer_arrived, &simulator);
+  thrlab_wait_for_customers(customer_arrived, &simulator);
 
-    thrlab_cleanup();
-    cleanup(&simulator);
+  thrlab_cleanup();
+  cleanup(&simulator);
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
